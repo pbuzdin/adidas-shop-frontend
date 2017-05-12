b83e006921918d841752a0bd278ed653cd101bf2
Pavel <iboong@gmail.com>
2017-05-12 23:27:56 +0300

    added filter component

---
+++ 2/src/ShoesCategory/Content/Filter.js
@@ -0,0 +1,33 @@
+import React from 'react';
+//import ReactDOM from 'react-dom';
+import './style.css';
+
+const FilterSelect = (props) => (
+  <button>{props.children}</button>
+)
+
+export default () => {
+  return (
+    <div className="filter row">
+      <div className="gender col4">
+        <img src={require("./img/filter.svg")} className="filter-icon" alt="" />
+        <div className="gender-switch">
+          <FilterSelect>Man</FilterSelect>
+          <FilterSelect>Woman</FilterSelect>
+        </div>
+      </div>
+      <div className="sizes col8">
+        <span className="size-title">size</span>
+        <div className="size-line">
+          <FilterSelect>36</FilterSelect>
+          <FilterSelect>37</FilterSelect>
+          <FilterSelect>38</FilterSelect>
+          <FilterSelect>39</FilterSelect>
+          <FilterSelect>40</FilterSelect>
+          <FilterSelect>41</FilterSelect>
+          <FilterSelect>42</FilterSelect>
+        </div>
+      </div>
+    </div>
+  );
+}
--- 2/src/ShoesCategory/Content/index.js
+++ 2/src/ShoesCategory/Content/index.js
@@ -1,26 +1,12 @@
 import React from 'react';
 //import ReactDOM from 'react-dom';
 import './style.css';
-//import Logo from './Logo';
+import Filter from './Filter';
 
 export default () => {
   return (
     <div className="content">
-      <div className="filter row">
-        <div className="gender col4">
-          <img src={require("./img/filter.png")} className="filter-icon" alt="" />
-          <div className="gender-switch">
-            <button className="active">Man</button>
-            <button>Woman</button>
-          </div>
-        </div>
-        <div className="sizes col8">
-          <span className="size-title">size</span>
-          <div className="size-line">
-            <button>36</button><button>37</button><button>38</button><button>39</button><button>40</button><button className="active">41</button><button>42</button>
-          </div>
-        </div>
-      </div>
+      <Filter />
 
       <div className="products-grid row">
         <div className="col4">

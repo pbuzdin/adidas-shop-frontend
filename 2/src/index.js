7549089b996d5826942e9b8f250fea1601325fa2
Pavel <iboong@gmail.com>
2017-05-12 04:16:09 +0300

    переименовал файлы как требуется

--- 2/src/index.js
+++ 2/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
 import './index.css';
-import ShoesCategory from './ShoesCategory/ShoesCategory';
+import ShoesCategory from './ShoesCategory';
 
 ReactDOM.render( <
   ShoesCategory / > ,

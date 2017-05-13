15f66d2b5be51068398e4e6c7627462fbfd9d1a1
Pavel <iboong@gmail.com>
2017-05-13 19:40:31 +0300

    Made better routing

--- src/ShoesCategory/index.js
+++ src/ShoesCategory/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
 import './style.css';
+import './Content/style.css';
 import Sidebar from './Sidebar';
-import Content from './Content';
+import List from './Content/List';
+import Details from './Content/Details';
 
 export default () => {
   return (
-    <div className="container1">
-      <Sidebar />
-      <Content />
-    </div>
+    <Router>
+      <div className="container1">
+        <Sidebar />
+        <div className="content">
+          <Route exact path="/" component={List} />
+          <Route path="/item" component={Details} />
+        </div>
+      </div>
+    </Router>
   );
 }

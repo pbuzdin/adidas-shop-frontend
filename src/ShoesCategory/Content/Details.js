6e8c98c1deb19f664f63e41ddb3650e41596c2b1
Pavel <iboong@gmail.com>
2017-05-13 15:55:33 +0300

    added Details images via require

--- src/ShoesCategory/Content/Details.js
+++ src/ShoesCategory/Content/Details.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Badge from './Badge';
 
 export default () => {
   return (
@@ -7,7 +8,7 @@ export default () => {
         <div className="hover">
 
           <div className="left-info">
-            <h1 className="product-title">Ultra<br />Boost</h1>
+            <h1 className="product-title">Ultra Boost</h1>
             <button className="save">save</button>
           </div>
 
@@ -16,28 +17,20 @@ export default () => {
             <button className="color-switch color2"></button>
             <button className="color-switch color3"></button>
             <button className="color-switch color4"></button>
-            <span className="sale-badge">sale</span>
+            <Badge />
             <h2 className="price">170$</h2>
           </div>
 
         </div>
 
-        <img src="http://dev.iboong.com/adidas-shop-frontend/1/product3/img/product3-4.jpg" srcset="http://dev.iboong.com/adidas-shop-frontend/1/product3/img/product3-4@2x.jpg 2x, 
+        <img src={require("./img/product3-4.jpg")} src="http://dev.iboong.com/adidas-shop-frontend/1/product3/img/product3-4.jpg" srcset="http://dev.iboong.com/adidas-shop-frontend/1/product3/img/product3-4@2x.jpg 2x, 
           http://dev.iboong.com/adidas-shop-frontend/1/product3/img/product3-4@3x.jpg 3x" className="photo-show" />
 
         <div className="photo-tour">
-          <img src="http://dev.iboong.com/adidas-shop-frontend/1/product3/img/product3-1.jpg" srcset="http://dev.iboong.com/adidas-shop-frontend/1/product3/img/product3-1@2x.jpg 2x, 
-            http://dev.iboong.com/adidas-shop-frontend/1/product3/img/product3-1@3x.jpg 3x" className="photo-thumb" />
-
-          <img src="http://dev.iboong.com/adidas-shop-frontend/1/product3/img/product3-2.jpg" srcset="http://dev.iboong.com/adidas-shop-frontend/1/product3/img/product3-2@2x.jpg 2x, 
-            http://dev.iboong.com/adidas-shop-frontend/1/product3/img/product3-2@3x.jpg 3x" className="photo-thumb" />
-
-          <img src="http://dev.iboong.com/adidas-shop-frontend/1/product3/img/product3-3.jpg" srcset="http://dev.iboong.com/adidas-shop-frontend/1/product3/img/product3-3@2x.jpg 2x, 
-            http://dev.iboong.com/adidas-shop-frontend/1/product3/img/product3-3@3x.jpg 3x" className="photo-thumb" />
-
-          <img src="http://dev.iboong.com/adidas-shop-frontend/1/product3/img/product3-4.jpg" srcset="http://dev.iboong.com/adidas-shop-frontend/1/product3/img/product3-4@2x.jpg 2x, 
-            http://dev.iboong.com/adidas-shop-frontend/1/product3/img/product3-4@3x.jpg 3x" className="photo-thumb current" />
-
+          <img src={require("./img/product3-1.jpg")} className="photo-thumb" />
+          <img src={require("./img/product3-2.jpg")} className="photo-thumb" />
+          <img src={require("./img/product3-3.jpg")} className="photo-thumb" />
+          <img src={require("./img/product3-4.jpg")} className="photo-thumb current" />
           <div className="triple"></div>
           <a href="#" className="more-photos">see more photos</a>
 

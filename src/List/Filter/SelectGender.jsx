89c01c848230feb7090c933006eb7e2c9fcd2286
Pavel <iboong@gmail.com>
2017-05-14 17:01:49 +0300

    styled filters

--- src/List/Filter/SelectGender.jsx
+++ src/List/Filter/SelectGender.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const FilterSelect = props => (
-  <button className={props.className}>{props.children}</button>
-);
+const Gender = styled.div`
+  & img {
+    background: #ebebeb;
+    padding: 11px 7.5px;
+    float: left;
+    width: 55px;
+  }
+`;
+const SelectGender = styled.div`
+  margin-bottom: 36px;
+  margin-left: 84px;
+`;
+const FilterSelect = styled.button`
+  font-family: 'AvenirNext', Arial;
+  font-size: 24px;
+  color: #d6d6d6;
+  text-transform: uppercase;
+  border: 0;
+  background: transparent;
+  padding: 14px 7px;
+  cursor: pointer;
+  color: ${props => (props.active ? '#4d42f8' : '#d6d6d6')};
+`;
 
 export default () => (
-  <div className="gender">
-    <img src={require('./filter.svg')} className="filter-icon" alt="" />
-    <div className="gender-switch">
-      <FilterSelect className="active">Man</FilterSelect>
+  <Gender>
+    <img src={require('./filter.svg')} alt="" />
+    <SelectGender>
+      <FilterSelect active>Man</FilterSelect>
       <FilterSelect>Woman</FilterSelect>
-    </div>
-  </div>
+    </SelectGender>
+  </Gender>
 );

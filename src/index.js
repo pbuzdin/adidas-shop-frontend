b33eac5471a864fad846daa03fd685e2b74bde0a
Pavel <iboong@gmail.com>
2017-05-14 04:39:50 +0300

    renaming all to jsx

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 /* eslint-disable react/jsx-filename-extension */
-
+import 'normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'normalize.css';
 
-ReactDOM.render(<App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));

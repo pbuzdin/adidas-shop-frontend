9012b1ae6771b3096fe9bd7ef817d593f382be72
Pavel <iboong@gmail.com>
2017-05-14 17:09:02 +0300

    styled filter

--- src/List/Filter/SelectSize.jsx
+++ src/List/Filter/SelectSize.jsx
@@ -1,20 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const FilterSelect = props => (
-  <button className={props.className}>{props.children}</button>
-);
+const Sizes = styled.div`
+`;
+const FilterLabel = styled.span`
+  font-family: 'AvenirNext', Arial;
+  font-size: 24px;
+  color: #4d42f8;
+  text-transform: uppercase;
+  border: 0;
+  background: transparent;
+  padding: 14px 7px;
+  float: left;
+  margin-right: 18px;
+`;
+const FilterSelect = styled.button`
+  font-family: 'AvenirNext', Arial;
+  font-size: 24px;
+  color: #d6d6d6;
+  text-transform: uppercase;
+  border: 0;
+  background: transparent;
+  padding: 14px 7px;
+  cursor: pointer;
+  color: ${props => (props.active ? '#4d42f8' : '#d6d6d6')};
+`;
 
 export default () => (
-  <div className="gender">
-    <span className="size-title">size</span>
-    <div className="size-line">
-      <FilterSelect>36</FilterSelect>
-      <FilterSelect>37</FilterSelect>
-      <FilterSelect>38</FilterSelect>
-      <FilterSelect>39</FilterSelect>
-      <FilterSelect>40</FilterSelect>
-      <FilterSelect className="active">41</FilterSelect>
-      <FilterSelect>42</FilterSelect>
-    </div>
-  </div>
+  <Sizes>
+    <FilterLabel>size</FilterLabel>
+    <FilterSelect>36</FilterSelect>
+    <FilterSelect>37</FilterSelect>
+    <FilterSelect>38</FilterSelect>
+    <FilterSelect>39</FilterSelect>
+    <FilterSelect>40</FilterSelect>
+    <FilterSelect active>41</FilterSelect>
+    <FilterSelect>42</FilterSelect>
+  </Sizes>
 );

8cfe8b61f8d779d52affe5f5dc2a25d4f633dd38
Pavel <iboong@gmail.com>
2017-05-14 14:43:20 +0300

    styled Badge

--- src/Badge/index.jsx
+++ src/Badge/index.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
-import './style.css';
+import styled from 'styled-components';
 
-const Badge = props => <span className="sale-badge">{props.children}</span>;
-
-export default () => <Badge>sale</Badge>;
+const Badge = styled.span`
+  background: #ff5c5c;
+  font-family: 'AvenirNext', Arial;
+  font-size: 14px;
+  font-weight: bold;
+  text-transform: uppercase;
+  color: #ffffff;
+  padding: 6px 20px;
+  position: absolute;
+  margin-top: 7px;
+  margin-left: 243px;
+`;
+export default () => <Badge className="sale-badge">sale</Badge>;
